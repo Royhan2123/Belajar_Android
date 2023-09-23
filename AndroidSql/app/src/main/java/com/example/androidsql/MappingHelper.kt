9f4e9ec3202579fde172b37f8ab6be31d1c0fcfc
Royhan <royhankeren12@gmail.com>
2023-09-23 17:54:36 +0700

    folder

---
+++ AndroidSql/app/src/main/java/com/example/androidsql/MappingHelper.kt
@@ -0,0 +1,21 @@
+package com.example.androidsql
+
+import android.database.Cursor
+import com.example.androidsql.db.DatabaseContract
+import com.example.androidsql.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns.ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
