abbef2f79ccf73efa2db87e2af03cc14a2c8156a
Royhan <royhankeren12@gmail.com>
2023-09-23 10:39:48 +0700

    folder

--- AndroidBackgroundDataStore/app/build.gradle.kts
+++ AndroidBackgroundDataStore/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.androidcreatesettingactivity"
+    namespace = "com.example.createnoteapp"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.androidcreatesettingactivity"
+        applicationId = "com.example.createnoteapp"
         minSdk = 28
         targetSdk = 33
         versionCode = 1
@@ -41,8 +41,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // dataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 }
