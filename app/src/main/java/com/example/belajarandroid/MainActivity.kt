fc661e308cf8e5daf8700814d6681170031485fa
Royhan <royhankeren12@gmail.com>
2023-12-15 09:17:44 +0700

    MainActivity

---
+++ app/src/main/java/com/example/belajarandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.belajarandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugassubmission.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
