734d69dc7b89523b473cbd77b1dc3ddc98a04c74
Royhan <royhankeren12@gmail.com>
2024-01-25 18:44:33 +0700

    Ngirim Data Intent

--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.firstapplication
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.firstapplication.databinding.ActivityMainBinding
 import com.example.hero.Hero
 import com.example.person.Person
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+        if (result.resultCode == MoveForResultActivity.RESULT_CODE && result.data != null){
+            val selectValue = result.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE,0)
+            binding.tvResult.text = "Hasil : $selectValue"
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +29,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.btnMoveActivityData.setOnClickListener(this)
         binding.btnMoveActivityObject.setOnClickListener(this)
         binding.btnHero.setOnClickListener(this)
+        binding.btnDialNumber.setOnClickListener(this)
+        binding.btnMoveForResult.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -59,6 +71,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val intent = Intent(this@MainActivity,com.example.firstapplication.Hero::class.java)
                 intent.putExtra(com.example.firstapplication.Hero.USERHERO,hero)
                 startActivity(intent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "089130930101"
+                val phoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(phoneIntent)
+            }
+
+            R.id.btn_move_for_result -> {
+                val intent = Intent(this@MainActivity,MoveForResultActivity::class.java)
+                resultLauncher.launch(intent)
             }
         }
     }
