a71b1ec49ee9b631283d6c164452c952fe921ee5
Royhan <royhankeren12@gmail.com>
2024-01-26 21:20:12 +0700

    Adapter

--- FirstApplication/app/src/main/java/com/example/LatihanReycleview/ListHeroAdapter.kt
+++ FirstApplication/app/src/main/java/com/example/LatihanReycleview/ListHeroAdapter.kt
@@ -1,28 +1,28 @@
-package com.example.firstapplication
+package com.example.LatihanReycleview
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapplication.Hero
+import com.example.firstapplication.R
 
-class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView
-        .Adapter<ListHeroAdapter.ListViewHolder>() {
-
+class ListHeroAdapter(private val listHero:ArrayList<Hero>) : 
+        RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
     class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imgPhoto : ImageView = view.findViewById(R.id.img_item_photo)
-        val tvName :TextView = view.findViewById(R.id.tv_item_name)
-        val tvDescription :TextView = view.findViewById(R.id.tv_item_description)
+        val imgPhoto:ImageView = view.findViewById(R.id.img_item_photo)
+        val tvName:TextView = view.findViewById(R.id.tv_item_name)
+        val tvDescription:TextView = view.findViewById(R.id.tv_item_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-     val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero,parent,false)
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero,parent,false)
         return ListViewHolder(view)
     }
 
-    override fun getItemCount(): Int = listHero.size
+    override fun getItemCount(): Int  = listHero.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name,description,photo) = listHero[position]
--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,22 +8,23 @@ import com.example.firstapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val list = ArrayList<Hero>()
+    private val listHero = ArrayList<Hero>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recycleView.setHasFixedSize(true)
-
-        list.addAll(getListHeroes())
-        showRecyclerList()
+        listHero.addAll(getListHeroes())
+        showRecycleList()
     }
 
-    private fun getListHeroes() : ArrayList<Hero>{
+    @SuppressLint("Recycle")
+    private fun getListHeroes(): ArrayList<Hero> {
         val dataName = resources.getStringArray(R.array.data_name)
         val dataDescription = resources.getStringArray(R.array.data_description)
         val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+
         val listHero = ArrayList<Hero>()
         for (i in dataName.indices){
             val hero = Hero(dataName[i],dataDescription[i],dataPhoto.getResourceId(i,-1))
@@ -30,10 +32,10 @@ class MainActivity : AppCompatActivity() {
         }
         return listHero
     }
-
-    private fun showRecyclerList(){
+    private fun showRecycleList(){
         binding.recycleView.layoutManager = LinearLayoutManager(this)
-        val listHeroAdapter = ListHeroAdapter(list)
-        binding.recycleView.adapter = listHeroAdapter
+        val listAdapter = ListHeroAdapter(listHero)
+        binding.recycleView.adapter = listAdapter
     }
+
 }
