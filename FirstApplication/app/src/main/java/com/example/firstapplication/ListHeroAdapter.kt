69abb6440059e35b7a7bd59e71671a863448d81c
Royhan <royhankeren12@gmail.com>
2024-01-26 12:08:44 +0700

    adapter

--- FirstApplication/app/src/main/java/com/example/firstapplication/ListHeroAdapter.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/ListHeroAdapter.kt
@@ -1,24 +1,34 @@
 package com.example.firstapplication
 
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>(){
-    class ListViewHolder(view:View) : RecyclerView.ViewHolder(view) {
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView
+        .Adapter<ListHeroAdapter.ListViewHolder>() {
 
+    class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imgPhoto : ImageView = view.findViewById(R.id.img_item_photo)
+        val tvName :TextView = view.findViewById(R.id.tv_item_name)
+        val tvDescription :TextView = view.findViewById(R.id.tv_item_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+     val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero,parent,false)
+        return ListViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listHero.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (name,description,photo) = listHero[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
     }
 
 }
