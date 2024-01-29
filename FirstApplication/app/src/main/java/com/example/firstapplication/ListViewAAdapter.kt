0fdf6c8a80457c3531dee8ef722eacc6211e0eb6
Royhan <royhankeren12@gmail.com>
2024-01-29 14:32:20 +0700

    add Glide

---
+++ FirstApplication/app/src/main/java/com/example/firstapplication/ListViewAAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.firstapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ListViewAdapter (private val listHero:ArrayList<Hero>) :
+RecyclerView.Adapter<ListViewAdapter.ListViewHolder>(){
+
+    class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imgPhoto:ImageView = view.findViewById(R.id.img_item_photo)
+        val tvName:TextView = view.findViewById(R.id.tv_item_name)
+        val tvDescription:TextView = view.findViewById(R.id.tv_item_description)
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListViewHolder {
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero,parent,false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder:ListViewHolder, position: Int) {
+        val (name,description,photo) = listHero[position]
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .into(holder.imgPhoto)
+    }
+    override fun getItemCount(): Int = listHero.size
+
+}
--- FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,45 +1,15 @@
 package com.example.firstapplication
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firstapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val listHero = ArrayList<Hero>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recycleView.setHasFixedSize(true)
-        listHero.addAll(getListHeroes())
-        showRecycleList()
-
-    }
-
-    private fun showRecycleList() {
-        binding.recycleView.layoutManager = LinearLayoutManager(this)
-        val adapter = ListViewAdapter(listHero)
-        binding.recycleView.adapter = adapter
-    }
-
-    @SuppressLint("Recycle")
-    private fun getListHeroes (): ArrayList<Hero> {
-        val dataName = resources.getStringArray(R.array.data_name)
-        val dataDescription = resources.getStringArray(R.array.data_description)
-        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
-
-        val lisHero = ArrayList<Hero>()
-
-        for (i in dataName.indices){
-            val hero = Hero(dataName[i],dataDescription[i],dataPhoto.getResourceId(i,-1))
-            listHero.add(hero)
-        }
-
-        return lisHero
     }
 }
