d07729cb2e6cb90befb8bae63a9efbb79d666516
Royhan <royhankeren12@gmail.com>
2024-01-31 14:23:32 +0700

    fragment

---
+++ FundamentalApplication/app/src/main/java/com/example/fundamentalapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fundamentalapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fundamentalapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val supportFragment = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = supportFragment.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment){
+            supportFragment.beginTransaction()
+                .add(R.id.container,homeFragment,HomeFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+}
