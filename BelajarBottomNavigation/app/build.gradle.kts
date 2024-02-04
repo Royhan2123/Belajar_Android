c6a9b61443f8eb1128d344a3b42fc70714ad15d1
Royhan <royhankeren12@gmail.com>
2024-02-04 15:11:17 +0700

    adapter

--- BelajarBottomNavigation/app/build.gradle.kts
+++ BelajarBottomNavigation/app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -55,4 +54,5 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.15.0")
 }
---
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.belajarbottomnavigation
+
+data class Hero(
+    val name:String,
+    val description:String,
+    val image:String
+)
