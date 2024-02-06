cffc5e21ff193cfa1ab9934579c02b7df9c8c9a5
Royhan <royhankeren12@gmail.com>
2024-02-06 15:30:59 +0700

    api services

--- BelajarBottomNavigation/app/build.gradle.kts
+++ BelajarBottomNavigation/app/build.gradle.kts
@@ -55,7 +55,9 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.github.bumptech.glide:glide:4.15.0")
     implementation("com.loopj.android:android-async-http:1.4.9")
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
