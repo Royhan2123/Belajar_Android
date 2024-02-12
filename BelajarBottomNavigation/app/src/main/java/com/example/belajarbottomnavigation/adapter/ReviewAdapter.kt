c384e2410e0fb2b8a41d11c47d0712aeca701bec
Royhan <royhankeren12@gmail.com>
2024-02-12 18:25:46 +0700

    restaurantReponse

--- BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/adapter/ReviewAdapter.kt
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/adapter/ReviewAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.belajarbottomnavigation
+package com.example.belajarbottomnavigation.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
--- BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/notifications/NotificationsFragment.kt
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/notifications/NotificationsFragment.kt
@@ -1,11 +1,24 @@
 package com.example.belajarbottomnavigation.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.belajarbottomnavigation.adapter.ReviewAdapter
+import com.example.belajarbottomnavigation.data.response.CustomerReviewsItem
+import com.example.belajarbottomnavigation.data.response.Restaurant
+import com.example.belajarbottomnavigation.data.response.RestaurantResponse
+import com.example.belajarbottomnavigation.data.retrofit.ApiConfig
 import com.example.belajarbottomnavigation.databinding.FragmentNotificationsBinding
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Callback
+
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
@@ -28,5 +41,61 @@ class NotificationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.rvReview.layoutManager = layoutManager
+
+
+        val itemDecoration = DividerItemDecoration(requireContext(),layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
+
+        findRestaurant()
+    }
+
+    private fun findRestaurant() {
+        showLoading(true)
+        val client = ApiConfig.getApiServices().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : Callback<RestaurantResponse> {
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                showLoading(false)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        responseBody.restaurant?.let { setRestaurantData(it) }
+                        setReviewData(responseBody.restaurant?.customerReviews)
+                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+    private fun setRestaurantData(restaurant: Restaurant) {
+        binding.tvTitle.text = restaurant.name
+        binding.tvDescription.text = restaurant.description
+        Glide.with(requireContext())
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+            .into(binding.ivPicture)
+    }
+    private fun setReviewData(consumerReviews: List<CustomerReviewsItem?>?) {
+        val adapter = ReviewAdapter()
+        adapter.submitList(consumerReviews)
+        binding.rvReview.adapter = adapter
+        binding.edReview.setText("")
+    }
+
+
+    private fun showLoading(isLoading:Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 }
