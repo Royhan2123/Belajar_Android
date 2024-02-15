7e65a1be755d490253df2f6484b4c7d9b070d746
Royhan <royhankeren12@gmail.com>
2024-02-15 17:44:05 +0700

    account fragment

--- BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/account/AccountFragment.kt
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/account/AccountFragment.kt
@@ -6,13 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.belajarbottomnavigation.R
 import com.example.belajarbottomnavigation.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
     private var _binding : FragmentAccountBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: AccountViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,5 +23,31 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[AccountViewModel::class.java]
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.toString()
     }
 }
--- BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/account/AccountViewModel.kt
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/account/AccountViewModel.kt
@@ -3,5 +3,9 @@ package com.example.belajarbottomnavigation.ui.account
 import androidx.lifecycle.ViewModel
 
 class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var result = 0
+
+    fun calculate(width:String,height:String,length:String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
 }
