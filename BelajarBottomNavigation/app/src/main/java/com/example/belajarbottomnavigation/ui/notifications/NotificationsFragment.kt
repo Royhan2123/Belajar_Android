e9ef9bf5ff9969a1f7b1d096673dd31695174584
Royhan <royhankeren12@gmail.com>
2024-02-15 08:46:41 +0700

    restaurant response

--- BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/notifications/NotificationsFragment.kt
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/ui/notifications/NotificationsFragment.kt
@@ -8,17 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.example.belajarbottomnavigation.adapter.ReviewAdapter
-import com.example.belajarbottomnavigation.data.response.CustomerReviewsItem
-import com.example.belajarbottomnavigation.data.response.Restaurant
-import com.example.belajarbottomnavigation.data.response.RestaurantResponse
-import com.example.belajarbottomnavigation.data.retrofit.ApiConfig
 import com.example.belajarbottomnavigation.databinding.FragmentNotificationsBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
@@ -48,58 +38,5 @@ class NotificationsFragment : Fragment() {
 
         val itemDecoration = DividerItemDecoration(requireContext(), layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
-
-        findRestaurant()
-    }
-
-    private fun findRestaurant(){
-        showLoading(true)
-
-        val client = ApiConfig.getApiServices().getRestaurant(RESTAURANT_ID)
-
-        client.enqueue(object : Callback<RestaurantResponse> {
-            override fun onResponse(
-                call: Call<RestaurantResponse>,
-                response: Response<RestaurantResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful){
-                    val responseBody = response.body()
-                    if (responseBody != null){
-                        responseBody.restaurant?.let { setRestaurant(it) }
-                        setReviewData(responseBody.restaurant?.customerReviews)
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
-               Log.e(TAG,"onFailure ${t.message}")
-            }
-        })
-    }
-
-    private fun setRestaurant(restaurant: Restaurant) {
-        binding.tvTitle.text = restaurant.name
-        binding.tvDescription.text = restaurant.description
-        Glide.with(requireContext())
-            .load("$BASE_URL/${restaurant.pictureId}")
-            .into(binding.ivPicture)
-    }
-
-    private fun setReviewData(consumer: List<CustomerReviewsItem?>?) {
-        val adapter = ReviewAdapter()
-        adapter.submitList(consumer)
-        binding.rvReview.adapter = adapter
-        binding.edReview.setText("")
-    }
-
-    private fun showLoading(isLoading:Boolean) {
-        if (isLoading){
-            binding.progressBar.visibility = View.VISIBLE
-        } else {
-            binding.progressBar.visibility = View.GONE
-        }
     }
 }
