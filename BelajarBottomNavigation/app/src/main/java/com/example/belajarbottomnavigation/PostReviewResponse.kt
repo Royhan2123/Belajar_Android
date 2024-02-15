731b4c000905a60fce8d1f440f10ca50d3bca47b
Royhan <royhankeren12@gmail.com>
2024-02-15 09:34:40 +0700

    postReviewReponse

---
+++ BelajarBottomNavigation/app/src/main/java/com/example/belajarbottomnavigation/PostReviewResponse.kt
@@ -0,0 +1,15 @@
+package com.example.belajarbottomnavigation
+
+import com.example.belajarbottomnavigation.data.response.CustomerReviewsItem
+import com.google.gson.annotations.SerializedName
+
+data class PostReviewResponse(
+    @field:SerializedName("customerReviews")
+    val customerReviewsItem: List<CustomerReviewsItem>,
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+)
